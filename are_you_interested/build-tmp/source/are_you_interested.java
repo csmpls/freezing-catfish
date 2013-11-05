75db2d29e24c73d61b3d18b0182c2a210bdf37e5
csmpls <csmpls@airbears2-10-142-134-188.airbears2.1918.berkeley.edu>
2013-11-04 20:15:28 -0800

    all framework's set for making a decent logger

--- are_you_interested/build-tmp/source/are_you_interested.java
+++ are_you_interested/build-tmp/source/are_you_interested.java
@@ -76,7 +76,7 @@ public void setup() {
    noStroke();
 
 }
-
+ 
 public void draw() {
     
 		
@@ -274,6 +274,59 @@ public String getTrailingHTML() {
         "</div>\n" +
         "</body>\n" + 
 	"</html>";
+}
+BufferedWriter logger;
+
+public void setupLog() {
+  
+  try {
+    // set up html file for reviewing
+    File file = new File("log.csv");
+    file.createNewFile();
+    
+    //wipe file contents
+    PrintWriter wiper = new PrintWriter(file);
+    wiper.print("");
+    wiper.close();
+
+    // set up a buffered writer for it
+    FileWriter fileWritter = new FileWriter(file,true);
+    logger = new BufferedWriter(fileWritter);
+  }
+  
+  catch (Exception e) {
+    e.printStackTrace();
+  }
+}
+
+public void updateLog() {
+  String[] logline = { getTimestamp(), Float.toString(neurosky.attn), Float.toString(neurosky.med) };
+  try {
+    logger.write(get_csv_line(logline));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+}
+
+public String get_csv_line(String[] values) {
+
+  String ll = "";
+
+  for (int i = 0; i < values.length; i++) {
+    if (i < values.length-1) 
+      ll += values[i] + ",";
+    else 
+      ll += values[i] + "\n";
+
+  } return ll;
+}
+
+
+public String getTimestamp() {
+  int s = second(); 
+  int m = minute(); 
+  int h = hour(); 
+  return(h + ":" + m + ":" + s);
 }
 public void quit() {
   
